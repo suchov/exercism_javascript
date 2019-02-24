f1ef9a126e8dcb2497291063b833a1075a8a7892
suchov <artem.suchov@gmail.com>
2019-02-24 12:13:49 +0200

    modify with Suggestions of my home mentor

--- leap/leap.js
+++ leap/leap.js
@@ -1,7 +1,1 @@
-export const isLeap = (year) => {
-  if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
-    return true;
-  } else {
-    return false;
-  }
-};
+export const isLeap = year => (year % 4 === 0 && year % 100 !== 0) || !(year % 400);
