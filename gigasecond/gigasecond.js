5a9557a4d4f71af38136c2912bcb69cb72818e0b
suchov <artem.suchov@gmail.com>
2019-03-19 19:05:24 +0200

    get rid of extra const

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -1,6 +1,5 @@
 const GIGASECOND_IN_MILLIS = 1e9 * 1e3;
 
 export const gigasecond = (time) => {
-  const birthTime = time.getTime();
-  return new Date(birthTime + GIGASECOND_IN_MILLIS);
+  return new Date(time.getTime() + GIGASECOND_IN_MILLIS);
 };
